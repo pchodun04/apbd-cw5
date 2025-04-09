5e41f857394259cd90de16510cef3f01cec788ef
pchodun04 <pch.11@interia.pl>
2025-04-09 15:07:52 +0200

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,13 @@
+﻿using cw5.Models;
+
+namespace cw5;
+
+public static class Database
+{
+    public static List<Test> Tests = new List<Test>()
+    {
+        new Test() { Id = 1, Name = "Test1" },
+        new Test() { Id = 2, Name = "Test2" },
+        new Test() { Id = 3, Name = "Test3" }
+    };
+}
